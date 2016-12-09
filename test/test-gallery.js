482387b060a2e4bbfc0fcb2a826ea1f6cf019fe7
alexpoe22 <alexpoe22@gmail.com>
2016-12-09 09:10:13 -0800

    second test started

---
+++ test/test-gallery.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import chai from 'chai';
+
+const should = chai.should();
+
+import Gallery from '../js/components/gallery';
+
+describe('Gallery component', function() {
+	it('it pulls data for multiple images', function() {
+		const = 
+        0: {
+            url: "http://prod.static.raiders.clubs.nfl.com/nfl-assets/img/gbl-ico-team/OAK/logos/home/large.png " ,
+            description: "rando description 1"
+        },
+        1: {
+            url: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png",
+            description: "this photo is very photolike"
+        }
+    }
+	})
+})
