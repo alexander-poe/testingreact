1f5d8ddb4a3c9af6736b11d5a0e7bccdc00de774
Your Namecarlo <carlo.jesena@gmail.com>
2016-12-09 09:44:46 -0800

    first gallery test passing

--- test/test-gallery.js
+++ test/test-gallery.js
@@ -8,15 +8,21 @@ import Gallery from '../js/components/gallery';
 
 describe('Gallery component', function() {
 	it('it pulls data for multiple images', function() {
-		const = 
-        0: {
+
+		const images = [
+        {
             url: "http://prod.static.raiders.clubs.nfl.com/nfl-assets/img/gbl-ico-team/OAK/logos/home/large.png " ,
             description: "rando description 1"
         },
-        1: {
+        {
             url: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png",
             description: "this photo is very photolike"
         }
-    }
+    ]
+		const renderer = TestUtils.createRenderer();
+		renderer.render(<Gallery images={images} />);
+		const result = renderer.getRenderOutput();
+		result.props.className.should.equal('gallery');
+
 	})
 })
